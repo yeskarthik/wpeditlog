e9c8ce892b7596007595897a6d748d74099aeb60
Karthik Subramanian <itskarthik.s@gmail.com>
2012-01-12 00:13:37 +0530

    using pyirclib0.4.3 instead of writing from scratch

--- karmabot.py
+++ karmabot.py
@@ -1,33 +1,21 @@
-import sys
-import socket
+import pyirclib
 import string
+import sys
+import time
 
-HOST="irc.wikimedia.org"
-PORT=6667
-NICK="karbot"
-IDENT="karbot"
-REALNAME="KarthiksBot"
-CHANNEL="#en.wikipedia"
-readbuffer=""
+irc = pyirclib.Irclib('irc.wikimedia.org',6667)
+irc.setDebug = 1
+irc.login('client',username = 'karmabot')
+irc.join("#en.wikipedia")
 
-s=socket.socket()
-s.connect((HOST, PORT))
-s.send("NICK %s\r\n" % NICK)
-s.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
-s.send("JOIN %s\r\n" %CHANNEL)
-file=open("logs.txt","w")
+#irc.privmsg("#en.wikipedia","hi future developers!")
 
+def parsemessage(msg):
+  if msg['event'] == "PRIVMSG" and str(msg['text']) == "!NAMES":
+    print irc.names()
 while 1:
-    readbuffer=readbuffer+s.recv(1024)
-    temp=string.split(readbuffer, "\n")
-    readbuffer=temp.pop( )
+  message = irc.getmessage()
+  print message
+  parsemessage(message)
    
-
-    for line in temp:
-        decodedline = ""
-        line=string.rstrip(line)
-#        line=string.split(line)
-        if(line[0]=="PING"):
-            s.send("PONG %s\r\n" % line[1])
-        file.write((line)+"\r\n")
-        
+   
